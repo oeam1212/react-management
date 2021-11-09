8d8277155b0f501ec656da9c770555713f9e3e25
영현 <oeam1212@gmail.com>
2021-11-09 09:24:36 +0900

    Create React Project

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+## Management System
+This project made for CRM.
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+//JSX 문법을 따른다.
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="gray-background">
+      <img src={logo} lat="logo" />
+      <h2>Let's develop management system!</h2>
     </div>
   );
 }
